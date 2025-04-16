5191d1de576d31318e4e64da4d17f3cac3c07f94
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-04-16 15:45:15 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,7 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := portrait_mdpi
+TW_THEME := landscape_mdpi
 TW_ROTATION := 270
 TW_EXTRA_LANGUAGES := false
 TW_USE_INPUT_EVENTS := true
